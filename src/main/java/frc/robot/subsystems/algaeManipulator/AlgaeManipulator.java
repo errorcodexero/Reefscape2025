1dcb641688474b456e4f4edf2b50e6a31a6422d4
amukh0 <153405984+amukh0@users.noreply.github.com>
2025-01-18 14:52:13 -0800

    updated AlgaeManipulator structure

--- src/main/java/frc/robot/subsystems/algaeManipulator/AlgaeManipulator.java
+++ src/main/java/frc/robot/subsystems/algaeManipulator/AlgaeManipulator.java
@@ -1,8 +1,10 @@
 package frc.robot.subsystems.algaeManipulator;
 
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import frc.simulator.engine.ISimulatedSubsystem;
+import frc.simulator.utils.SettingsValue;
 
-public class AlgaeManipulator extends SubsystemBase{
+public class AlgaeManipulator extends SubsystemBase implements ISimulatedSubsystem{
     private AlgaeManipulatorIO io_; 
     private AlgaeManipulatorIOInputsAutoLogged inputs_ = new AlgaeManipulatorIOInputsAutoLogged();
 
@@ -14,5 +16,10 @@ public class AlgaeManipulator extends SubsystemBase{
      public void periodic(){
         io_.updateInputs(inputs_);
      }
+
+    @Override
+    public SettingsValue getProperty(String name) {
+        return null;
+    }
 }
 
---
+++ src/main/java/frc/robot/subsystems/algaeManipulator/AlgaeManipulatorConstants.java
@@ -0,0 +1,27 @@
+package frc.robot.subsystems.algaeManipulator;
+
+import static edu.wpi.first.units.Units.KilogramSquareMeters;
+
+import edu.wpi.first.units.measure.MomentOfInertia;
+
+public class AlgaeManipulatorConstants {
+
+    public static final double kGearRatio = 1.0;
+    public static final MomentOfInertia kMOI = KilogramSquareMeters.of(0.001);
+
+    public class Roller {
+        public static class PID {
+
+        }
+    }
+
+    public class Hinge {
+        public static class PID {
+            
+        }
+
+        public class Positions {
+
+        }
+    }
+}
