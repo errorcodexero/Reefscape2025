65df9e7a2517d5345b1bdf71feb50473e12f1db7
Pranav Kausik <pranav.kausikbs@gmail.com>
2025-01-20 19:47:52 -0800

    Fixed build errors

---
+++ src/main/java/frc/robot/subsystems/climber/Climber.java
@@ -0,0 +1,20 @@
+/*This is the ClimberSubsystem class. It is going to be used to manage 
+what the climber is going to do on the robot. The climber is what the
+robot is going to use to go up the deep cage. This code is incomplete
+ */
+
+package main.java.frc.robot.subsystems.Climber;
+
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+
+public class Climber extends SubsystemBase {
+
+  public ClimberSubsystem() {
+
+  }
+
+  @Override
+  public void periodic() {
+  
+  }
+}
