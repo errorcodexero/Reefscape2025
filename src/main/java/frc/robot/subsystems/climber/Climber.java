285ef7eb43ebf29fe751fa04dd3287398fcd1f62
Pranav Kausik <pranav.kausikbs@gmail.com>
2025-01-25 09:47:40 -0800

    Added logic for commands and states as comments

--- src/main/java/frc/robot/subsystems/climber/Climber.java
+++ src/main/java/frc/robot/subsystems/climber/Climber.java
@@ -1,11 +1,7 @@
-/*This is the ClimberSubsystem class. It is going to be used to manage 
-what the climber is going to do on the robot. The climber is what the
-robot is going to use to go up the deep cage. This code is incomplete
-
-Notes on how Climber works (Fork and Spaghetti method): 
-Robot drives into cage at slight angle, 2 hooks grab onto one side of cage, 
-rotates climber mechanism into the robot such that the chain is pointing into
-the robot, allowing the robot to essentially hover over the ground.
+/*Fork and spaghetti - Drive into cage, hooks hook onto bar, climber twists chain and cage into robot, 
+so hover off ground. 
+Commands - DeployClimber, ExecuteClimb
+States(enum) - IDLE, DeployClimber, WaitToHook, Climb
  */
 
  package frc.robot.subsystems.climber;
