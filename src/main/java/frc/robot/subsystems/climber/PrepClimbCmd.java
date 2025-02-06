4ce8c7a74552317d20b75be927288ca3cffc22a5
Pranav Kausik <pranav.kausikbs@gmail.com>
2025-02-05 20:42:25 -0800

    Simplified GoTo position for climber and began work on PrepClimb

--- src/main/java/frc/robot/subsystems/climber/PrepClimbCmd.java
+++ src/main/java/frc/robot/subsystems/climber/PrepClimbCmd.java
@@ -1,15 +1,18 @@
 package frc.robot.subsystems.climber;
 
-import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
+import frc.robot.subsystems.climber.ClimberSubsystem;
+import frc.robot.subsystems.climber.ClimberPositionCmd.ClimberTarget;
+import frc.robot.subsystems.climber.ClimberConstants;
+//import frc.robot.subsystems.funnel.FunnelSubsystem;
+//import frc.robot.susbsystems.funnel.FunnelConstants;
 
 
 public class PrepClimbCmd extends SequentialCommandGroup {
-    private ClimberSubsystem climber_;
-    private PrepClimbCmdState PrepClimbCmdState_;
-
-    private enum PrepClimbCmdState{
-
+    public PrepClimbCmd(ClimberSubsystem climberSubsystem) {
+        addCommands(
+            new ClimberPositionCmd(climberSubsystem, ClimberTarget.PrepareToClimb)
+        );
     }
 
 }
