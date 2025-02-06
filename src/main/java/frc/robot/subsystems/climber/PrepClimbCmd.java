fb042f8fe495174629a23462135eea8d7f91c150
Pranav Kausik <pranav.kausikbs@gmail.com>
2025-02-05 19:25:39 -0800

    Fixed reviews from Bryce

--- src/main/java/frc/robot/subsystems/climber/PrepClimbCmd.java
+++ src/main/java/frc/robot/subsystems/climber/PrepClimbCmd.java
@@ -3,6 +3,7 @@ package frc.robot.subsystems.climber;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
 
+
 public class PrepClimbCmd extends SequentialCommandGroup {
     private ClimberSubsystem climber_;
     private PrepClimbCmdState PrepClimbCmdState_;
