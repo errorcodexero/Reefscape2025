73c2104f6716cd51ca351a6c5fae308dd6935646
anvisingh12 <anvi3rcs@gmail.com>
2025-02-09 16:47:27 -0800

    add CollectCoralCmd, StowArmElevatorCmd, and PlaceCoralCmd

--- src/main/java/frc/robot/subsystems/manipulator/ManipulatorConstants.java
+++ src/main/java/frc/robot/subsystems/manipulator/ManipulatorConstants.java
@@ -15,7 +15,7 @@ public class ManipulatorConstants {
         public static final int kMotorCANID = 2; 
 
         // if motor is inverted 
-        public static final boolean kInverted = false; 
+        public static final boolean kInverted = true; 
 
         public static final double kGearRatio = 0; 
 
@@ -46,14 +46,26 @@ public class ManipulatorConstants {
         public class ThruBoreEncoder {
             // the encoder mapper needs double values, so these constants don't use the Units library
             // robot min and max are in degrees
-            public static final double kRobotMax = 360; 
-            public static final double kRobotMin = 0; 
-            public static final double kEncoderMax = 1; 
-            public static final double kEncoderMin = 0; 
+            public static final double kRobotMax = 179; 
+            public static final double kRobotMin = -90; 
+            public static final double kEncoderMax = 0; 
+            public static final double kEncoderMin = 1; 
             public static final double kRobotCalibrationValue = 0; 
-            public static final double kEncoderCalibrationValue = 0;
+
+            // different encoder calibration values based on the robot being used
+            public static final double kEncoderCalibrationValuePractice = 0;
+            public static final double kEncoderCalibrationValueComp = 0;
 
             public static final int kEncoderSource = 0; 
+        }
+
+        public class Positions {
+            public static final Angle kStow = Degrees.of(0);
+            public static final Angle kPlaceL1 = Degrees.of(0); 
+            public static final Angle kPlaceL2 = Degrees.of(0); 
+            public static final Angle kPlaceL3 = Degrees.of(0); 
+            public static final Angle kPlaceL4 = Degrees.of(0);
+            public static final Angle kKickbackAngle = Degrees.of(-10); 
         }
     }
 
@@ -91,6 +103,15 @@ public class ManipulatorConstants {
             public static final double kMaxVelocity = 0.0;
             public static final double kMaxAcceleration = 0.0;
             public static final double kJerk = 0.0;
+        }
+
+        public class Positions {
+            public static final Distance kStow = Meters.of(0);
+            public static final Distance kPlaceL1 = Meters.of(0); 
+            public static final Distance kPlaceL2 = Meters.of(0); 
+            public static final Distance kPlaceL3 = Meters.of(0); 
+            public static final Distance kPlaceL4 = Meters.of(0); 
+
         }
     }
 
