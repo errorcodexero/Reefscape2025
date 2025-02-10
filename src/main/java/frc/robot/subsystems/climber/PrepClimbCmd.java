83d5ac8a82b7a72a29bf4da60667a991a1cfdec0
Pranav Kausik <pranav.kausikbs@gmail.com>
2025-02-10 15:50:42 -0800

    Continuued work on PrepClimb and cleaned up ClimberPos command

--- src/main/java/frc/robot/subsystems/climber/PrepClimbCmd.java
+++ src/main/java/frc/robot/subsystems/climber/PrepClimbCmd.java
@@ -4,8 +4,8 @@ import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
 import frc.robot.subsystems.climber.ClimberSubsystem;
 import frc.robot.subsystems.climber.ClimberPositionCmd.ClimberTarget;
 import frc.robot.subsystems.climber.ClimberConstants;
-//import frc.robot.subsystems.funnel.FunnelSubsystem;
-//import frc.robot.susbsystems.funnel.FunnelConstants;
+import frc.robot.subsystems.funnel.DeployFunnelCmd;
+import frc.robot.subsystems.funnel.FunnelConstants;
 
 
 public class PrepClimbCmd extends SequentialCommandGroup {
