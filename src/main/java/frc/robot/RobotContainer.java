b182904b42a7936ee683273fe677f035271e985d
blaze-developer <bryceblazegaming@gmail.com>
2025-08-26 19:44:23 -0700

    Update

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -185,7 +185,7 @@ public class RobotContainer {
                     try {
                         grabber_ = new GrabberSubsystem(new GrabberIOHardware());
                     } catch (Exception ex) {
-                        subsystemCreateException(ex);
+                        // subsystemCreateException(ex);
                     }
 
                     try {
